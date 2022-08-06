a013d848d9aeb6783a3e6d0c77abc06ad1847b15
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2022-08-06 15:22:08 -0300

    Implement producer

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"math/rand"
+	"time"
 
+	"github.com/ajtfj/producer-consumer/eventbuffer"
+)
+
+const (
+	MAX_CONSUMERS = 100
+	MAX_EVENTS    = 1000
+	CAP           = 1
+)
+
+func generateEvent() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(1000)
+}
+
+func producer(eb *eventbuffer.EventBuffer) {
+	for i := 0; i < MAX_EVENTS; i++ {
+		event := generateEvent()
+		eb.Produce(event)
+	}
+}
+
+func main() {
+	eventBuffer := eventbuffer.NewEventBuffer(CAP)
+	go producer(eventBuffer)
 }
