bebcad74bf0c3ac87b475b626d150f4069a87f5a
Anselmo José Tavares Ferreira Junior <anselmo.jtfj@gmail.com>
2022-08-06 16:24:30 -0300

    Implement consumer

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/ajtfj/producer-consumer/eventbuffer"
@@ -19,12 +20,24 @@ func producer(eb *eventbuffer.EventBuffer, wg *sync.WaitGroup) {
 	}
 }
 
+func consumer(eb *eventbuffer.EventBuffer, ch *chan int) {
+	event := eb.Consume()
+	fmt.Println(event)
+	<-*ch
+}
+
 func main() {
 	eventBuffer := eventbuffer.NewEventBuffer(CAP)
 
 	producerWg := sync.WaitGroup{}
 	producerWg.Add(1)
 	go producer(eventBuffer, &producerWg)
+
+	chConsumer := make(chan int, MAX_CONSUMERS)
+	for i := 0; i < MAX_EVENTS; i++ {
+		chConsumer <- i
+		go consumer(eventBuffer, &chConsumer)
+	}
 
 	producerWg.Wait()
 }
